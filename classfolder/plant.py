c75c88f7028e1dab6a9885274f35be8a35de4dc5
Your Name <you@example.com>
2024-04-11 18:51:11 -0400

    :)

---
+++ classfolder/plant.py
@@ -0,0 +1,14 @@
+class Plant:
+    def __init__(self,name, waterfreq, ideallight, height, idealtemp, idealsoil, idealdrain, nutrientneeds, bloomtime, idealplanttime, pests):
+        self.name = name
+        self.waterfreq = waterfreq
+        self.ideallight = ideallight
+        self.height = height
+        self.idealtemp = idealtemp
+        self.idealsoil = idealsoil
+        self.idealdrain = idealdrain
+        self.nutrientneeds = nutrientneeds
+        self.bloomtime = bloomtime
+        self.idealplanttime = idealplanttime
+        self.pests = pests
+        
---
+++ classfolder/tasktoolbar.py
@@ -0,0 +1,8 @@
+class Tasktoolbar:
+    
+    def __init__(self,notifnum, notificationonoff, notiflist):
+        self.notifnum = notifnum
+        self.notification = notificationonoff
+        self.notiflist = notiflist
+        
+        
